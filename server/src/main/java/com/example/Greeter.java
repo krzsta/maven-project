f0b12fea2cd8e2fbecb8a9f4d05010ef6f6650a9
krzsta <stanowski.k@gmail.com>
2017-06-10 12:29:00 +0200

    fixed checstyle warrnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,18 +5,19 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+	/**
+	 * This is a constructor.
+	 */
+	public Greeter() {
 
-  }
-  
-  /*
-   * @param moe the...
-   * @return greeting ...
-   */  
-  public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+	}
+
+	/**
+	 * @param moe
+	 *            the...
+	 * @return greeting ...
+	 */
+	public final String greet(final String someone) {
+		return String.format("Hello, %s!", someone);
+	}
 }
